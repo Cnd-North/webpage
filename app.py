ac881039236f0680aeef2b2b34b8b1cb8628bafb
Cnd-North <gerrit.vr12@gmail.com>
2022-03-22 19:45:33 -0700

    Initial header section

---
+++ app.py
@@ -0,0 +1,14 @@
+# Gerrit van Rensburg
+# 2022-03-22 
+# Streamlit web-app
+
+
+import streamlit as st
+
+st.set_page_config(page_title="Gerrit's space on the web", page_icon=":pizza:", layout="wide")
+
+# Header Section
+st.subheader("You've arrived - Welcome! :pineapple:")
+st.title("Multi-Disciplinary Problem Solver")
+st.write("I am driven by my curiousity currently transition from Excel to Python")
+
