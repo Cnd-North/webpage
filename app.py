5b693a982c415bb839e95723f5d7d2edddc2d08f
Cnd-North <gerrit.vr12@gmail.com>
2022-03-22 19:59:21 -0700

    Added in a divider, text and link to github

--- app.py
+++ app.py
@@ -8,7 +8,33 @@ import streamlit as st
 st.set_page_config(page_title="Gerrit's space on the web", page_icon=":pizza:", layout="wide")
 
 # Header Section
-st.subheader("You've arrived - Welcome! :pineapple:")
-st.title("Multi-Disciplinary Problem Solver")
-st.write("I am driven by my curiousity currently transition from Excel to Python")
+with st.container():
+    st.subheader("You've arrived - Welcome! :pineapple:")
+    st.title("Multi-Disciplinary Problem Solver")
+    st.write("I am driven by my curiousity currently transition from Excel to Python")
 
+
+# What I do
+with st.comntainer():
+    st.write("---")  # insort horizontal divider line
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+        Currently working on a few projects - most of them are within python.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            """
+        )
+        st.write("[Github profile >] (https://github.com/Cnd-North)")
+
+        
